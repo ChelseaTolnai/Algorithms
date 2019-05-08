64fd1628489792cb0f1fb0b4829d2b4af07ad15f
ChelseaTolnai <ChelseaTolnai@gmail.com>
2019-05-08 16:58:32 -0400

    Completed recipe_batches.py

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -2,9 +2,18 @@
 
 import math
 
-def recipe_batches(recipe, ingredients):
-  pass 
-
+def recipe_batches(recipe, ingredients):        #Overall - O(n)
+  batches = None                                          #O(1)
+  for k, v in recipe.items():                             #O(n)
+    has_item_amount = ingredients.get(k)                  #O(1)
+    if not has_item_amount or has_item_amount < v:
+      return 0                                            #O(1)
+    else:
+      current_item_batches = has_item_amount // v         #O(1)
+      if not batches or current_item_batches < batches:   
+        batches = current_item_batches                    #O(1)
+  return batches                                          #O(1)
+      
 
 if __name__ == '__main__':
   # Change the entries of these dictionaries to test 
