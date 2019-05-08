3f0cd79a52c0da2a9d8319bdd40797ff8fe92bd0
ChelseaTolnai <ChelseaTolnai@gmail.com>
2019-05-08 16:09:13 -0400

    Completed stock_prices.py

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -2,8 +2,18 @@
 
 import argparse
 
-def find_max_profit(prices):
-  pass
+def find_max_profit(prices):        #Overall - O(n)
+  buy = max(prices[1:])                       #O(n)
+  sell = min(prices[:prices.index(buy)])      #O(n)
+  profit = buy - sell                         #O(1)
+  if profit > 0:                              
+    return profit                             #O(1)
+  else:
+    for i in range(len(prices)-1):            #O(n)
+      current_profit = prices[i+1]-prices[i]  #O(1)
+      if current_profit > profit:             
+        profit = current_profit               #O(1)
+    return profit                             #O(1)
 
 
 if __name__ == '__main__':
