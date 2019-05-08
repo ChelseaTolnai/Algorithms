ff73d32a13fec8193edb8ae209f2dcd9c9a72a08
ChelseaTolnai <ChelseaTolnai@gmail.com>
2019-05-08 19:59:45 -0400

    Refactor stock_prices.py to account for additional test cases whereby max profit may not be from difference of the max num in array. Slower runtime but correct outcome.

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -1,19 +1,30 @@
 #!/usr/bin/python
 
 import argparse
+import math
 
-def find_max_profit(prices):        #Overall - O(n)
-  buy = max(prices[1:])                       #O(n)
-  sell = min(prices[:prices.index(buy)])      #O(n)
-  profit = buy - sell                         #O(1)
-  if profit > 0:                              
-    return profit                             #O(1)
-  else:
-    for i in range(len(prices)-1):            #O(n)
-      current_profit = prices[i+1]-prices[i]  #O(1)
-      if current_profit > profit:             
-        profit = current_profit               #O(1)
-    return profit                             #O(1)
+def find_max_profit(prices):  # Overall - O(n^2)
+  profit = -math.inf                    # O(1)
+  while len(prices) >= 2:               # O(n)
+    sell = prices[0]                    # O(1)
+    buy = max(prices[1:])               # O(n)
+    if buy - sell > profit:
+        profit = buy - sell             # O(1)
+    prices = prices[1:]                 # O(1)
+  return profit                         # O(1)
+
+# def find_max_profit(prices):        #Overall - O(n)
+#   buy = max(prices[1:])                       #O(n)
+#   sell = min(prices[:prices.index(buy)])      #O(n)
+#   profit = buy - sell                         #O(1)
+#   if profit > 0:                              
+#     return profit                             #O(1)
+#   else:
+#     for i in range(len(prices)-1):            #O(n)
+#       current_profit = prices[i+1]-prices[i]  #O(1)
+#       if current_profit > profit:             
+#         profit = current_profit               #O(1)
+#     return profit                             #O(1)
 
 
 if __name__ == '__main__':
