aebae86fee8fdb1a319d5a05a6ec942b8aef58ce
ChelseaTolnai <ChelseaTolnai@gmail.com>
2019-05-09 15:30:48 -0400

    Completed rps.py.

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -2,8 +2,19 @@
 
 import sys
 
-def rock_paper_scissors(n):
-  pass 
+
+def rock_paper_scissors(n):     # Overall - O(n)
+  rps = ['rock', 'paper', 'scissors']     # O(1)
+  plays = []                              # O(1)
+  
+  def rps_results_adder(n, play):
+    if n == 0:
+      return plays.append(play)           # O(1)
+    for i in rps:                         # O(3)
+      rps_results_adder(n-1, play + [i])  # O(n)
+
+  rps_results_adder(n, [])                # O(1)
+  return plays                            # O(1)
 
 
 if __name__ == "__main__":
