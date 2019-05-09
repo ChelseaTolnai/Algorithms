4ebeaa1ac7197825045791543b85f2a7d7e2374c
ChelseaTolnai <ChelseaTolnai@gmail.com>
2019-05-09 17:56:08 -0400

    Completed making_change.py

--- making_change/making_change.py
+++ making_change/making_change.py
@@ -1,9 +1,21 @@
 #!/usr/bin/python
 
 import sys
+# amount = [0+, 5+, 10+, 15+, 20+, 25+, 30+, 35+, 40+, 45+, 50+, 55+, ]
+# cache =  [1,  2,  4,   6,   9,   13,  18,  24,  31,  39,  50,  62]
 
-def making_change(amount, denominations):
-  pass 
+def making_change(amount, denominations): # Overall - O(n)
+  cache = [1 for i in range(amount + 1)]            # O(1)                
+  if amount < 0:
+    return 0                                        # O(1)
+  elif amount < 5:
+    return 1                                        # O(1)
+  else:
+    for coin in denominations[1:]:                  # O(4)
+      for higher_amount in range(coin, amount + 1): # O(n)
+        difference = higher_amount - coin           # O(1)
+        cache[higher_amount] += cache[difference]   # O(1)
+    return cache[amount]                            # O(1)
 
 
 if __name__ == "__main__":
